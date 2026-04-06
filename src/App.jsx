882fa180c643837e68ec443533fe3921c258d191
Sneh <kumarsneh35@gmail.com>
2026-04-06 16:55:01 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-purple-400">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
