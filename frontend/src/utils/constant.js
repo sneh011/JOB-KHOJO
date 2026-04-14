012a3b6e38e7d0622ab8047ea3686e95564d0b8a
Sneh <kumarsneh35@gmail.com>
2026-04-15 00:00:36 +0530

    connected frontend to render backend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:3000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company";
-export const SAVE_JOB_API_END_POINT="http://localhost:3000/api/v1/user";
+export const USER_API_END_POINT="https://job-search-rr82.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-search-rr82.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-search-rr82.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-search-rr82.onrender.com/api/v1/company";
+export const SAVE_JOB_API_END_POINT="https://job-search-rr82.onrender.com/api/v1/user";
