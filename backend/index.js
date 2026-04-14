b7b95073384262ec4c511fda7d3c42f3c3363e50
Sneh <kumarsneh35@gmail.com>
2026-04-15 00:09:39 +0530

    fixed cors fully

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,12 @@ const app = express();
 
 // 🔥 CORS FIRST (IMPORTANT)
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://job-khojo-delta.vercel.app"
+  ],
+  credentials: true
 }));
-
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
